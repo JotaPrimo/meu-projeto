08c2823fbb8eef37fbf84223e2fa923a39ca715a
JotaPrimo <gestald118@gmail.com>
2023-11-07 23:22:53 -0400

    Primeiros passos com JSF e Primefaces #44: ConnectionFactory

---
+++ src/main/java/br/com/medicamento/dao/ConnectionFactory.java
@@ -0,0 +1,14 @@
+package br.com.medicamento.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ConnectionFactory {
+	
+	private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("CursoPU");
+
+	private static EntityManager getEntityManager() {
+		return entityManagerFactory.createEntityManager();
+	}
+}
--- src/main/java/br/com/medicamento/model/Medicamento.java
+++ src/main/java/br/com/medicamento/model/Medicamento.java
@@ -2,6 +2,7 @@ package br.com.medicamento.model;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Medicamento implements Serializable, Base {
 
@@ -18,10 +19,65 @@ public class Medicamento implements Serializable, Base {
 	private BigDecimal preco;
 
 	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Medicamento other = (Medicamento) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
 	public Long getId() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getApresentacao() {
+		return apresentacao;
+	}
+
+	public void setApresentacao(String apresentacao) {
+		this.apresentacao = apresentacao;
+	}
+
+	public String getLaboratorio() {
+		return laboratorio;
+	}
+
+	public void setLaboratorio(String laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	
 	
 	
 	
